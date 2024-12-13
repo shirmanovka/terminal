c3e31108b3711a476cbf3f8c8e43c05cca1dc87c
Kirill <187307451+shirmanovka@users.noreply.github.com>
2024-12-13 13:48:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,16 @@ df = pd.read_excel('Карта рынка fix.xlsx', skiprows=1)
     # Преобразование колонки 'Погашение' в формат datetime
 df['Погашение'] = pd.to_datetime(df['Погашение'], format='%d-%m-%Y', errors='coerce')
 
-    # Выбор месяца и года для фильтрации
-month_to_filter = st.selectbox("Выберите месяц для фильтрации", options=available_months)
-year_to_filter = st.selectbox("Выберите год для фильтрации", options=available_years)
 
- # Проверка, какие месяцы и годы доступны для фильтрации
+# Проверка, какие месяцы и годы доступны для фильтрации
 available_months = df['Погашение'].dt.month.unique()
 available_years = df['Погашение'].dt.year.unique()
 
- 
+# Выбор месяца и года для фильтрации
+month_to_filter = st.selectbox("Выберите месяц для фильтрации", options=available_months)
+year_to_filter = st.selectbox("Выберите год для фильтрации", options=available_years)
+
+
     # Фильтрация по месяцу и году
 filtered_df = df[(df['Погашение'].dt.month == month_to_filter) & (df['Погашение'].dt.year == year_to_filter)]
 
